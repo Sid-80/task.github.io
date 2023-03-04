16787819bda1a6940edddd04d0081fc427379fba
Sid-80 <101277141+Sid-80@users.noreply.github.com>
2023-03-04 13:47:38 +0530

    api

--- main.js
+++ main.js
@@ -7,19 +7,20 @@ async function myFunc () {
   let userInput = document.querySelector("#userInput");
   let userInputWarn = document.querySelector("#warning");
   const accessToken = "a84456f7119c21b5e61ff84398e5bb83";
+  const pi = 139397;
+  const click_id = "a1b2c3";
   let ip = "";
   const reqApi = "https://pin.trafficcompany.com/api/v1/sendpin";
   if(userInput.value === ""){
     return userInputWarn.innerHTML = "Enter the number !!";
   }else{
     try {
-      const res = await fetch('https://geolocation-db.com/json/');
+      const res = await fetch('https://api.ipify.org/?format=json');
       const data = await res.json();
-      ip = data.IPv4;
-      const inputApi = `${reqApi}?access-token=${accessToken}&ip=${ip}&msisdn=${userInput.value}`;
+      ip = data.ip;
+      const inputApi = `${reqApi}?access-token=${accessToken}&ip=${ip}&msisdn=${userInput.value}&pi=${pi}&click_id=${click_id}`;
       const res2 = await fetch(inputApi);
       const data2 = await res2.json();
-      console.log(data2);
       if(data2.status === "error"){
         return userInputWarn.innerHTML = `Message : ${data2.message}`;
       }else{
